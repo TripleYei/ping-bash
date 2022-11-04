5f45df1b3b534e257c26bf17a251329136e015f9
TripleYei <58819156+TripleYei@users.noreply.github.com>
2022-11-04 22:32:12 +0100

    Add files via upload

---
+++ ping.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat << EOF
+
+        .__            ____   
+______  |__|  ____    / ___\  
+\____ \ |  | /    \  / /_/  > 
+|  |_> >|  ||   |  \ \___  /  
+|   __/ |__||___|  //_____/   
+|__|             \/           
+                      Triple yei        
+
+
+EOF
+
+echo "Introduce el dominio"
+read dominio
+ping $dominio
